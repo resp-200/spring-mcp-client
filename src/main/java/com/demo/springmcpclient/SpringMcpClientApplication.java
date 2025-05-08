3bd472af466786543573fa94be7ecc41889710d9
zhanghaoyu01 <zhanghaoyu01@zhuanzhuan.com>
2025-05-08 10:24:46 +0800

    提示词

--- src/main/java/com/demo/springmcpclient/SpringMcpClientApplication.java
+++ src/main/java/com/demo/springmcpclient/SpringMcpClientApplication.java
@@ -21,6 +21,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -32,42 +33,57 @@ public class SpringMcpClientApplication {
     }
 
 
-//    @Bean
-//    public CommandLineRunner predefinedQuestions(List<McpSyncClient> mcpSyncClients,
-//                                                 ConfigurableApplicationContext context) {
-//
-//        return args -> {
-//            McpSyncClient mcpSyncClient = mcpSyncClients.get(0);
-//            McpSchema.ListToolsResult listToolsResult = mcpSyncClient.listTools();
-//            System.out.println(listToolsResult);
-//
-//            McpSchema.CallToolResult callToolResult = mcpSyncClient.callTool(new McpSchema.CallToolRequest("getWeather", Map.of("city", "北京")));
-//            System.out.println(callToolResult);
+    /** 测试功能
+     * @param mcpSyncClients mcpClient，自动注入
+     * @param context context
+     * @return
+     */
+    @Bean
+    public CommandLineRunner callTool(List<McpSyncClient> mcpSyncClients,
+                                                 ConfigurableApplicationContext context) {
+
+        return args -> {
+            System.out.println("普通client调用方法------------------------------------");
+            // 获取mcp-client
+            McpSyncClient mcpSyncClient = mcpSyncClients.get(0);
+            McpSchema.ListToolsResult listToolsResult = mcpSyncClient.listTools();
+            System.out.println(listToolsResult);
+
+            McpSchema.CallToolResult callToolResult = mcpSyncClient.callTool(new McpSchema.CallToolRequest("getWeather", Map.of("city", "北京")));
+            System.out.println(callToolResult);
+            // 结束会话
 //            context.close();
-//        };
-//    }
+        };
+    }
 
 
 
     @Bean
-    public CommandLineRunner predefinedQuestions(ChatClient.Builder chatClientBuilder,
+    public CommandLineRunner callToolByLLM(ChatClient.Builder chatClientBuilder,
                                                  ToolCallbackProvider toolCallbackProvider,
                                                  ConfigurableApplicationContext context) {
 
         return args -> {
+            System.out.println("基于spring-ai，llm调用方法--------------------------------");
             // 获取工具列表
             Gson gson = new Gson();
-            System.out.println(toolCallbackProvider);
-            // 用户输出
+            for (FunctionCallback toolCallback : toolCallbackProvider.getToolCallbacks()) {
+                System.out.println(toolCallback.getName() + " " + toolCallback.getDescription() + " " + toolCallback.getInputTypeSchema());
+            }
+
+            // 模拟用户输入的信息
             var chatClient = chatClientBuilder
                     .defaultTools(toolCallbackProvider)
                     .build();
             String userInput = "北京今天天气怎么样";
             System.out.println("用户问: " + userInput);
+
+            // 包装请求LLM
             String content = chatClient.prompt(userInput).call().content();
             System.out.println("AI回答: " + gson.toJson(content));
 
 
+            // 结束会话
             context.close();
         };
     }
